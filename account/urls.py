14bf86a15e7283c48d91814924b997cc2329066a
Diyorbek <diyorbek0143@gmail.com>
2025-03-11 17:44:26 +0500

    commit

--- account/urls.py
+++ account/urls.py
@@ -8,10 +8,6 @@ router = DefaultRouter()
 # router.register(r'users', UserViewSet, basename='user')  
 
 urlpatterns = [
-    path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
-    path('change-password/', ChangePasswordView.as_view(), name='change-password'),
-    path('users/', UserListView.as_view(), name='user-list'),
-    path('users/<str:token>/', UserRetrieveUpdateDestroyView.as_view(), name='user-detail'),
     path('', include(router.urls)), 
 ]
